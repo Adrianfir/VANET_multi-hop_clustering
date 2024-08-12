82f662c7c1232fdbd6fcfb806fbe35ca8a79dc8b
Adrianfir <pfirouzmakan@gmail.com>
2024-08-12 10:39:24 -0400

    working on the comparison part

---
+++ utils/util_pmc.py
@@ -0,0 +1,47 @@
+"""
+This is the utils file including the small functions
+"""
+__author__: str = "Pouya 'Adrian' Firouzmakan"
+__all__ = [
+
+]
+
+import numpy as np
+import random
+import haversine as hs
+from linked_list import LinkedList
+from scipy import spatial
+import time
+from PIL import Image
+from io import BytesIO
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import os
+import cv2
+import re
+
+import utils.util as util
+
+
+def be_ch():
+
+
+
+def calculate_etx():
+    return 1
+
+
+def calculate_nfollow():
+    n_follow = 0
+    return n_follow
+
+
+def calculate_lltm():
+    lltm = 0
+    return lltm
+
+
+def calculate_pri():
+    pri = 0
+    return pri
+
